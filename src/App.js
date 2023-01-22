976f15c87075ac1926b45664ef39bc10b10503f2
NikolayS <nikolay.sedelkov@gmail.com>
2023-01-22 15:31:08 +0300

    task2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+function SetJsonFetch({url, opts=""}){
+  console.log(url);
+  const [data     , setData   ] = useState();
+  const [error    , setError  ] = useState();
+  const [isLoading, setLoading] = useState(true);
+  useEffect( () => {
+  const fetchData = async () => {
+    try{
+      const response = await fetch(url);
+      if (!response.ok) setError(new Error(response.statusText));
+      else              setData(await response.json());
+    }catch(e){setError(e)}
+    finally{setLoading(false)}
+  }
+  fetchData();
+  }, [url])
+
+  if(isLoading) return <p>{`${url}: Loading`}</p>
+  if(error) return <p>{`${url}: error(${error})`}</p>
+  return (
+    <>
+      {JSON.stringify(data)}
+    </>
+  )
+  return [data, error, isLoading];
+}
 
 function App() {
+
+  const [url, setUrl] = useState();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="urlButton" onClick={()=>setUrl("data")}>data</div>
+      <div className="urlButton" onClick={()=>setUrl("error")}>error</div>
+      <div className="urlButton" onClick={()=>setUrl("loading")}>loading</div>
+      {url&&<SetJsonFetch url={`http://localhost:7070/${url}`}/>}
     </div>
   );
 }
